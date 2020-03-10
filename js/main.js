20b7b7915f22c59eaf413266326026a17350c89d
aymen.hamed@gmail.com <aym.hamed@gmail.com>
2020-03-10 18:53:47 +0100

    [FIX]

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@
   Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
 });
+$('#installApp').on('click',function (params) {
+   // Hide the app provided install promotion
+   hideMyInstallPromotion();
+   // Show the install prompt
+   deferredPrompt.prompt();
+   // Wait for the user to respond to the prompt
+   deferredPrompt.userChoice.then((choiceResult) => {
+     if (choiceResult.outcome === 'accepted') {
+       console.log('User accepted the install prompt');
+     } else {
+       console.log('User dismissed the install prompt');
+     }
+   })
+})
   $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");

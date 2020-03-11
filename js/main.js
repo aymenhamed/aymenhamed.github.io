36be2c7fe4c08da2566046096b91e25dc9f6db5a
aymen.hamed@gmail.com <aym.hamed@gmail.com>
2020-03-11 10:22:54 +0100

    [FIX]

--- js/main.js
+++ js/main.js
@@ -3,30 +3,30 @@
   Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
 });
-let deferredPrompt;
+// let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-});
-$('#installApp').on('click',function (params) {
-   // Hide the app provided install promotion
-   hideMyInstallPromotion();
-   // Show the install prompt
-   deferredPrompt.prompt();
-   // Wait for the user to respond to the prompt
-   deferredPrompt.userChoice.then((choiceResult) => {
-     if (choiceResult.outcome === 'accepted') {
-       console.log('User accepted the install prompt');
-     } else {
-       console.log('User dismissed the install prompt');
-     }
-   })
-})
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent the mini-infobar from appearing on mobile
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI notify the user they can install the PWA
+//   //showInstallPromotion();
+// });
+// $('#installApp').on('click',function (params) {
+//    // Hide the app provided install promotion
+//    //hideMyInstallPromotion();
+//    // Show the install prompt
+//    deferredPrompt.prompt();
+//    // Wait for the user to respond to the prompt
+//    deferredPrompt.userChoice.then((choiceResult) => {
+//      if (choiceResult.outcome === 'accepted') {
+//        console.log('User accepted the install prompt');
+//      } else {
+//        console.log('User dismissed the install prompt');
+//      }
+//    })
+// });
   $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
